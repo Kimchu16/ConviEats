35be53a84738cb7256eef56773d2a4be1930371a
Kimchu16 <kimberlylibranza@gmail.com>
2025-05-22 20:20:51 +0900

    Implemented scanner controller
    - Also with clean separation of logic

---
+++ lib/features/scanner/scanner_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'scanner_controller.dart';
+import '/core/services/product_lookup.dart';
+
+class BarcodeScannerPage extends StatefulWidget {
+  const BarcodeScannerPage({super.key});
+
+  @override
+  State<BarcodeScannerPage> createState() => _BarcodeScannerPageState();
+}
+
+class _BarcodeScannerPageState extends State<BarcodeScannerPage>{
+    final ScannerController _scannerController = ScannerController();
+
+    void _handleScan(String code){
+        final info = ProductLookup.getInfo(code);
+
+        showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+                title: const Text('Barcode Scanned'),
+                content: Text('Code: $code\n\n$info'),
+                actions: [
+                    TextButton(
+                        onPressed: (){
+                            Navigator.pop(context);
+                            _scannerController.reset(); // Re-enable scanning
+                        },
+                        child: const Text('OK'))
+                ],
+            ),
+        );
+    }
+
+    @override
+    Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Scan a Product')),
+      body: MobileScanner(
+        controller: _scannerController.scanner,
+        onDetect: (capture) {
+          _scannerController.onDetect(capture, _handleScan);
+        },
+      ),
+    );
+  }
+}
