7fdc3faafb9098934637d645eebabcbce8a134cf
Kimchu16 <kimberlylibranza@gmail.com>
2025-05-22 19:43:11 +0900

    Fixed file naming methods
    - Also started working on scanner_controller

---
+++ lib/features/scanner/scanner_controller.dart
@@ -0,0 +1,21 @@
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class ScannerController {
+  final MobileScannerController scanner = MobileScannerController();
+  bool _hasScanned = false;
+
+  /// Called when barcode is detected from the scanner.
+  void onDetect(BarcodeCapture capture, Function(String code) onScan) {
+    if (_hasScanned) return;
+
+    final String? code = capture.barcodes.first.rawValue;
+    if (code == null) return;
+
+    _hasScanned = true;
+    onScan(code);
+  }
+
+  void reset() {
+    _hasScanned = false;
+  }
+}
