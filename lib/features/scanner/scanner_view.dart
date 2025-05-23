5f7be44ca24220c2b4ee7a4e33d8f5d28ea0999f
Kimchu16 <kimberlylibranza@gmail.com>
2025-05-23 22:05:40 +0900

    Scanner files cleaned up
    - Removed scanner_page as it is no longer needed since there is an actual UI page
    - Added comments on the remaining scanner files

--- lib/features/scanner/scanner_view.dart
+++ lib/features/scanner/scanner_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'scanner_controller.dart';
 
+// a reusable Scanner widget, which can be embedded in any screen.
 class ScannerView extends StatefulWidget {
   final Function(String barcode) onScanComplete;
 
@@ -16,6 +17,7 @@ class _ScannerViewState extends State<ScannerView> {
 
   @override
   Widget build(BuildContext context) {
+    // activates the device camera and starts scanning.
     return MobileScanner(
       controller: _scannerController.scanner,
       onDetect: (capture) {
