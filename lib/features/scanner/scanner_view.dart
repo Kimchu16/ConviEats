184a482dfe0d2e3f6d7e63600fe8a02ea6271d2e
Kimchu16 <kimberlylibranza@gmail.com>
2025-05-23 15:50:34 +0900

    Update scanner_view.dart

--- lib/features/scanner/scanner_view.dart
+++ lib/features/scanner/scanner_view.dart
@@ -2,19 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'scanner_controller.dart';
 
-class ScannerView extends StatelessWidget {
+class ScannerView extends StatefulWidget {
   final Function(String barcode) onScanComplete;
 
   const ScannerView({super.key, required this.onScanComplete});
 
   @override
-  Widget build(BuildContext context) {
-    final ScannerController _scannerController = ScannerController();
+  State<ScannerView> createState() => _ScannerViewState();
+}
 
+class _ScannerViewState extends State<ScannerView> {
+  final ScannerController _scannerController = ScannerController();
+
+  @override
+  Widget build(BuildContext context) {
     return MobileScanner(
       controller: _scannerController.scanner,
       onDetect: (capture) {
-        _scannerController.onDetect(capture, onScanComplete);
+        _scannerController.onDetect(capture, widget.onScanComplete);
       },
     );
   }
